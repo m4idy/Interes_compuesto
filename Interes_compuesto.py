d1ffbbb0b14baee8adfeac24d7eac34d7d286c6d
m4idy <maidy2234507@correo.uis.edu.co>
2023-04-01 08:32:54 -0500

    codigo

--- Interes_compuesto.py
+++ Interes_compuesto.py
@@ -4,7 +4,10 @@ t=0
 D=2*C
 
 while(C<D):
+    
     C=(C+0.05*C)
     t=t+1
+    print("para el mes "+ str(t) + " el valor es " + str(C))
 
-print("se duplica en " + str(t))
+
+print("se duplica en el mes  " + str(t))
